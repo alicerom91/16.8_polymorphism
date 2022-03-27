2e4312cc92aae3985ed2601ecaca7b39f61a9915
alicerom91 <68548318+alicerom91@users.noreply.github.com>
2022-03-27 15:37:07 +0300

    Exception added

--- rectangle.py
+++ rectangle.py
@@ -14,6 +14,14 @@ class Square:
     def get_area_square(self):
         return self.a ** 2
 # vozvrashenije v stepen **2 v kvadrat
+#Iskljuchenije
+class NonPositiveDigitException(ValueError):
+    pass
+
+class Square:
+    def __init__(self, a):
+        if a < 0:
+            raise NonPositiveDigitException('Неправильно указанна сторона квадрата')
 
 
 class Circle:
@@ -22,3 +30,4 @@ class Circle:
 
     def get_area_circle(self):
         return (self.r ** 2) * 3.14
+
